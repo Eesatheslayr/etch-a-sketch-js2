518c1a1e084636e0d8483c034db74f7a4b8ade20
Eesatheslayr <eesag4@gmail.com>
2024-09-20 00:05:30 +0200

    Add 16x16 grid creation loop in JS DOM and h1 'Etch A Sketch' with CSS styling
    - Implemented a loop to dynamically generate a 16x16 grid of squares in the JS DOM.
    - Added an h1 element with the title 'Etch A Sketch'.
    - Styled the h1 with appropriate CSS for visual presentation.
    - Applied CSS styles to the grid and squares for layout and appearance.

---
+++ etch.js
@@ -0,0 +1,13 @@
+const square=document.querySelector('#square');
+
+function grid(){
+    for (let i=0; i > 16; i++)
+    square.style.width ='100px'
+    square.style.height= '1000px'
+    square.style.background ='green'
+    square,style.display='flex';
+
+}
+
+console.log(grid());
+
