ee17bc0b4168802a0e774f6b4d5abbc87b6e6da0
Eesatheslayr <eesag4@gmail.com>
2024-09-23 22:31:38 +0200

    figured out how to loop 'sqaure' element.
    - in etch.js added a loop to grid() and looped it 256 times to get a 16x16.
    - in style.css changed height and width of .sqaure to make the grid 16x16.
    - in style.css changed .container display to inline-block.
    - still need to figure out how to make flexbox work in .container, using inline-block for now to create grid.

--- etch.js
+++ etch.js
@@ -2,16 +2,14 @@ const container=document.querySelector('#container');
 
 
 function grid(){
+    for (let i=0; i<256;i++){
         const square=document.createElement('square');
         square.classList.add('container');
-        square.style.height='100px';
-        square.style.width='100px';
-        square.style.backgroundColor='palegreen';
+        square.style.height='85px';
+        square.style.width='85px';
+        square.style.borderStyle='solid'
+        square.style.borderColor='blue';
         container.appendChild(square);
-
+    }
 }
-
 grid();
-grid();
-grid();
-
