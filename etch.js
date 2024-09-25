09369bdcbd93c5a521c3a73f095a5fce140eee73
Eesatheslayr <eesag4@gmail.com>
2024-09-25 21:47:50 +0200

    Added event listener.
    - added event listner 'mouseover' that changes the color of the 'square' div when the mouse hovers over it.
    - Trying to fix 'container' div so that size can be adjusted to make grid smaller.

--- etch.js
+++ etch.js
@@ -2,14 +2,32 @@ const container=document.querySelector('#container');
 
 
 function grid(){
-    for (let i=0; i<256;i++){
+    for (let i=0; i<90;i++){
         const square=document.createElement('square');
         square.classList.add('container');
-        square.style.height='85px';
-        square.style.width='85px';
-        square.style.borderStyle='solid'
-        square.style.borderColor='blue';
+        square.style.flex='';
+        square.style.height='80px';
+        square.style.width='100px'
+        square.style.borderStyle='dotted';
+        square.style.borderColor='darkgreen';
+        square.addEventListener('mouseover', ()=>{
+          square.style.background='black';
+        })
         container.appendChild(square);
     }
 }
 grid();
+
+/*function click(){
+  container.style.background='black';
+}
+
+container.addEventListener('onmouseover', click);
+    
+
+
+const mouse=document.createElement('mouse');
+container.appendChild(mouse);
+
+
+mouse.addEventListener(onmousedown, */
