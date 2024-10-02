b55b3a69658e36b7905c7b254c25ca38a4830580
Eesatheslayr <eesag4@gmail.com>
2024-10-02 22:18:39 +0200

    Major changes made...
    - Removed old function that created grid. did not work properly for different grid sizes.
    - Removing this function got rid of previous margin between grid row problem.
    - facing new problem of making a function that can make any size grid fit into 'container'

--- etch.js
+++ etch.js
@@ -1,17 +1,47 @@
 const container=document.querySelector('#container');
+const grid=document.createElement('div');
+container.appendChild(grid);
 
 
 
-function grid(){
-    for (let i=0; i<256;i++){
+function newRow(){
+  for (let i=0; i <10; i++){
+    const row=document.createElement('newDiv');
+    row.className='row';
+    row.addEventListener('click', ()=>{
+      row.style.background='red';
+    })
+    container.appendChild(row);
+  }
+  const column=document.createElement('newDive');
+    column.className='column';
+    row.appendChild(column);
+  column.addEventListener('click', ()=>{
+    column.style.background='orange'
+  })
+}
+
+newRow();
+
+/*function newColumn(){
+  for (let i=0; i<10; i++){*/
+   
+   /* column.addEventListener('click', ()=>{
+      column.style.background='blue';
+    })*/
+   
+  
+
+/*newColumn()
+/*function grid(){
+    for (let i=0; i<1000;i++){
         const square=document.createElement('div');
         square.classList.add('container');
-        square.style.display='flex';
         square.style.flexGrow='1';
-        square.style.height='35px';
+        square.style.height='35px'; 
         square.style.width='35px';
         square.style.borderStyle='solid';
-        square.style.borderColor='white';
+        square.style.borderColor='black';
         square.addEventListener('click', ()=>{
           square.style.background='red';
         })
@@ -19,19 +49,20 @@ function grid(){
     }
 }
 grid();
+*/
+
+
+
+
+
 
 const button=document.querySelector('#btn');
 button.textContent='CHANGE GRID SIZE';
 
-/*button.addEventListener('click', ()=>{
-  prompt('hello');
-})*/
-
-
 function gridPrompt(){
   let max=prompt('How many sqaures per side would you like? (MAX:100)')
   if (max==='1')
-  return container.style.background='blue';
+  return grid(0);
 else{
   return container.style.background='orange';
 }
