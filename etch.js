7cceaee708b144ffb02155d30c762ef102703b81
Eesatheslayr <eesag4@gmail.com>
2024-10-08 16:06:54 +0200

    FIXED GRID
    - Fixed grid problem by completey removing all functons that the loops resided in.
    - Created an outter/inner loop.
    - Outter loop created 'column' added the className linking it to style.css and appended it to 'container'
    - Inner loop created 'row' added the className linking it to the style.css and appened it to 'column'
    - added .row to style.css with its style properties.
    - added display:flex; to .column in style.css to make 'column' a flex container.
    - Need to addEventListner back to etch.js
    - Need to make function that changes grid size depending on userChoice.

--- etch.js
+++ etch.js
@@ -1,60 +1,17 @@
 const container=document.querySelector('#container');
-const grid=document.createElement('div');
-container.appendChild(grid);
 
+for (let i=0; i<10; i++){
+const column=document.createElement('div');
+column.className='column';
+container.appendChild(column);
 
-
-function newRow(){
-  for (let i=0; i <10; i++){
-    const row=document.createElement('newDiv');
-    row.className='row';
-    row.addEventListener('click', ()=>{
-      row.style.background='red';
-    })
-    container.appendChild(row);
-  }
-  const column=document.createElement('newDive');
-    column.className='column';
-    row.appendChild(column);
-  column.addEventListener('click', ()=>{
-    column.style.background='orange'
-  })
+for (let i=0; i<10; i++){
+  const row=document.createElement('newDiv');
+  row.className='row';
+  column.appendChild(row);
 }
-
-newRow();
-
-/*function newColumn(){
-  for (let i=0; i<10; i++){*/
-   
-   /* column.addEventListener('click', ()=>{
-      column.style.background='blue';
-    })*/
-   
-  
-
-/*newColumn()
-/*function grid(){
-    for (let i=0; i<1000;i++){
-        const square=document.createElement('div');
-        square.classList.add('container');
-        square.style.flexGrow='1';
-        square.style.height='35px'; 
-        square.style.width='35px';
-        square.style.borderStyle='solid';
-        square.style.borderColor='black';
-        square.addEventListener('click', ()=>{
-          square.style.background='red';
-        })
-        container.appendChild(square);
-    }
 }
-grid();
-*/
-
-
-
-
-
+ 
 
 const button=document.querySelector('#btn');
 button.textContent='CHANGE GRID SIZE';
