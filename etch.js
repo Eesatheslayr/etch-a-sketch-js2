3274c3da52f52157e07ab9af934c7b09ba6a9e49
Eesatheslayr <eesag4@gmail.com>
2024-10-10 14:19:37 +0200

    Added Prompt.
    - Added a prompt to grid() function that asks user for new grid size and creates new grid based on user choice.
    - centered #container and #btn in style.css

--- etch.js
+++ etch.js
@@ -1,34 +1,29 @@
 const container=document.querySelector('#container');
 
-for (let i=0; i<50; i++){
+
+function grid(){
+let max=prompt('How many sqaures per side would you like? (MAX:100)')
+for (let i=0; i<max; i++){
 const column=document.createElement('div');
 column.className='column';
 container.appendChild(column);
 
-
-for (let i=0; i<50; i++){
+for (let i=0; i<max; i++){
   const row=document.createElement('div');
   row.className='row';
   row.addEventListener('mouseover', ()=>{
-    row.style.background='blue';
+    row.style.background='yellow';
   })
   column.appendChild(row);
-} 
+} }
 }
- 
 
-
+grid();
 
 const button=document.querySelector('#btn');
 button.textContent='CHANGE GRID SIZE';
 
-function gridPrompt(){
-  let max=prompt('How many sqaures per side would you like? (MAX:100)')
-  if (max==='1')
-  return grid(0);
-else{
-  return container.style.background='orange';
-}
-}
+
+
 
 
